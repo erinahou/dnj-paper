0718be0690ac8f90a0181421d6ad8f79b3905cb5
Erina Ho <erina.hou@gmail.com>
2024-06-03 08:35:31 +1000

    Created reusable components with props

--- src/pages/Materials/Kanten.jsx
+++ src/pages/Materials/Kanten.jsx
@@ -1,7 +1,14 @@
+import React from 'react'
+import MaterialInfo from "../../components/MaterialInfo"
+
 export default function Kanten() {
+  let bodyText = `Agar may have been discovered in Japan in 1658 by Mino Tarōzaemon (美濃 太郎左衞門), an innkeeper in current Fushimi-ku, Kyoto who, according to legend, was said to have discarded surplus seaweed soup (Tokoroten) and noticed that it gelled later after a winter night's freezing. Over the following centuries, agar became a common gelling agent in several Southeast Asian cuisines. We use it as a finish on our paper cloth.`
+
+  console.log()
   return(
-    <>
-      <h1>About Kanten</h1>
-    </>
+    <MaterialInfo
+      title="Agar Agar / Kanten / 寒天"
+      info={bodyText}
+    />
   )
 }
